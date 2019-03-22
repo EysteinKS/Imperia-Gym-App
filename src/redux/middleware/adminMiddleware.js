114524b64af49b26c4d853b9d11fac4a3d5e74d6
Eystein <eystein.kolsto@gmail.com>
2019-03-22 20:29:05 +0100

    Minor commit for changing workstation

--- src/redux/middleware/adminMiddleware.js
+++ src/redux/middleware/adminMiddleware.js
@@ -2,10 +2,14 @@ import { firestore as f } from "../../firebase"
 import * as admin from "../actions/adminActions"
 import _ from "lodash"
 
+//TODO: CREATE SAGAS!
+
+
 //DISPATCHES UPDATE_CATEGORY ON ADD_EXERCISE
 export const addExerciseToCategory = store => next => action => {
   if(action.type === "ADD_EXERCISE"){
-    const { location, id, active } = action.payload
+    let actionCopy = _.cloneDeep(action)
+    const { location, id, active } = actionCopy.payload
     const newLocation = f.makeExercisesRef(f.makeCategoriesRef(location)) + `.${id}`
     let updatePayload = _.update(action.payload, newLocation, active)
     store.dispatch(admin.updateCategory(updatePayload))
